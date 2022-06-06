b4ef5133443b99c506a6ce3357b24c92236510d4
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-06-06 09:06:27 -0400

    update metric styles

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -10,38 +10,38 @@ function Picker({ stockDetails }) {
       <div>
         <h3>{stockDetails.Name}</h3>
         {valueIndex > 0 && <h4>{stockDetails.Symbol} is <span style={{color: "red", fontWeight: "bold"}}>UNDERVALUED</span> and is trading below its 200 MA by {valueIndexPercent}</h4>}
-        <div style={{display: "flex", justifyContent: "space-between", flexWrap: "wrap", margin: "2%"}}>
+        <div style={{display: "flex", justifyContent: "space-around", flexWrap: "wrap", margin: "2%"}}>
           <div style={{width: "25%", minWidth: "200px", display: "flex", justifyContent: "space-between"}}>
-            <div>
+            <div className="metrics">
               <p>Mkt Cap:</p>
               <p>PE (FWD):</p>
               <p>PE (TTM):</p>
             </div>
-            <div>
+            <div className="metrics">
               <p>${Math.floor(parseInt(stockDetails.MarketCapitalization) / Math.pow(10, 9))} b</p>
               <p>{stockDetails.ForwardPE}</p>
               <p>{stockDetails.TrailingPE}</p>
             </div>
           </div>
-          <div style={{width: "25%", display: "flex", justifyContent: "space-between"}}>
-          <div>
+          <div style={{width: "25%", minWidth: "200px", display: "flex", justifyContent: "space-between"}}>
+          <div className="metrics">
               <p>EPS:</p>
               <p>P/Sales:</p>
               <p>P/Book:</p>
             </div>
-            <div>
+            <div className="metrics">
               <p>{stockDetails.EPS}</p>
               <p>{stockDetails.PriceToSalesRatioTTM}</p>
               <p>{stockDetails.PriceToBookRatio}</p>
             </div>
           </div>
-          <div style={{width: "35%", display: "flex", justifyContent: "space-between"}}>
-          <div style={{minWidth: "100px"}}>
+          <div style={{width: "35%", minWidth: "200px", display: "flex", justifyContent: "space-between"}}>
+          <div className="metrics">
               <p>50 SMA:</p>
               <p>200 SMA:</p>
               <p>Price:</p>
             </div>
-            <div>
+            <div className="metrics">
               <p>{stockDetails["50DayMovingAverage"]}</p>
               <p>{stockDetails["200DayMovingAverage"]}</p>
               <p>272.02</p>
