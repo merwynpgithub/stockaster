5f224c46aebbf747f5b142a4f8a81bb4c4789f40
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-06-06 09:39:10 -0400

    update Picker component to add value condition

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -3,15 +3,16 @@ import priceConverter from '../components/helper/price-sorter';
 
 function Picker({ stockDetails, stockPrice }) {
   const sma200 = (Number(stockDetails["200DayMovingAverage"]));
-  const valueIndex = (sma200 - 272.02) / sma200;
-  const valueIndexPercent = parseInt(valueIndex * 100) + "%";
   const currentStockPrice = priceConverter(stockPrice);
+  const valueIndex = (sma200 - currentStockPrice) / sma200;
+  const valueIndexPercent = Math.abs(parseInt(valueIndex * 100)) + "%";
   return (
     <>
       
       <div>
         <h3>{stockDetails.Name}</h3>
-        {valueIndex > 0 && <h4>{stockDetails.Symbol} is <span style={{color: "red", fontWeight: "bold"}}>UNDERVALUED</span> and is trading below its 200 MA by {valueIndexPercent}</h4>}
+        {valueIndex > 0 && <h4>{stockDetails.Symbol} is <span style={{color: "red", fontWeight: "bold"}}>UNDERVALUED</span> and is trading below its 200 Day MA by {valueIndexPercent}</h4>}
+        {valueIndex <= 0 && <h4>{stockDetails.Symbol} is <span style={{color: "red", fontWeight: "bold"}}>NOT UNDERVALUED</span> and is trading around its 200 Day MA by {valueIndexPercent}</h4>}
         <div style={{display: "flex", justifyContent: "space-around", flexWrap: "wrap", margin: "2%"}}>
           <div style={{width: "25%", minWidth: "200px", display: "flex", justifyContent: "space-between"}}>
             <div className="metrics">
