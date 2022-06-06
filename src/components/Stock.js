fa6d5bbcc0e5b68a51bb9f44b49e95ae19e1394e
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-06-06 10:54:05 -0400

    add apple details

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -11,54 +11,80 @@ import msft_inc1 from '../data/msft_inc1';
 import msft_cf1 from '../data/msft_cf1';
 import msft_price from '../data/msft_price';
 
+import aapl_overview from '../data/aapl_overview';
+import aapl_bal1 from '../data/aapl_bal1';
+import aapl_inc1 from '../data/aapl_inc1';
+import aapl_cf1 from '../data/aapl_cf1';
+import aapl_price from '../data/aapl_price';
+
 import './styles/chart.css';
 
 function Stock() {
   const [horizon, setHorizon] = useState("quarterlyReports");
   const [ticker, setTicker] = useState("Microsoft");
+  const [overview, setOverview] = useState(msft_overview);
+  const [bal, setBal] = useState(msft_bal1);
+  const [inc, setInc] = useState(msft_inc1);
+  const [cf, setCf] = useState(msft_cf1);
+  const [price, setPrice] = useState(msft_price);
 
   function handleHorizonChange(e) {
     setHorizon(e.target.value);
   }
   function handleTickerChange(e) {
-    setHorizon(e.target.value);
+    setTicker(e.target.value);
+    if (e.target.value == "Microsoft") {
+      setTicker("Microsoft");
+      setOverview(msft_overview);
+      setBal(msft_bal1);
+      setInc(msft_inc1)
+      setCf(msft_cf1);
+      setPrice(msft_price);
+    }
+    if (e.target.value == "Apple") {
+      setTicker("Apple");
+      setOverview(aapl_overview);
+      setBal(aapl_bal1);
+      setInc(aapl_inc1)
+      setCf(aapl_cf1);
+      setPrice(aapl_price);
+    }
   }
   return (
     <>
     <select value={ticker} onChange={handleTickerChange}>
       <option value="Microsoft">Microsoft</option>
-      {/* <option value="Apple">Apple</option> */}
+      <option value="Apple">Apple</option>
     </select>
-      {ticker === "Microsoft" &&
-      <Picker stockDetails={msft_overview} stockPrice={msft_price}/>
-      }
+      
+      <Picker stockDetails={overview} stockPrice={price}/>
+      
       <select value={horizon} onChange={handleHorizonChange}>
         <option value="quarterlyReports">QUARTERLY</option>
         <option value="annualReports">ANNUAL</option>
       </select>
 
-      {ticker === "Microsoft" &&
+      
       <div style={{display: "flex", justifyContent: "space-around", flexWrap: "wrap"}}>
       <div className="chart">
-        <BarChart info={msft_inc1} name={"Microsoft"} type="revenue" horizon={horizon}/>
+        <BarChart info={inc} name={ticker} type="revenue" horizon={horizon}/>
       </div>
       <div className="chart">
-        <BarChart info={msft_inc1} name={"Microsoft"} type="net_income" horizon={horizon}/>
+        <BarChart info={inc} name={ticker} type="net_income" horizon={horizon}/>
       </div>
       <div className="chart">
-        <BarChart info={msft_inc1} name={"Microsoft"} type="ebitda" horizon={horizon}/>
+        <BarChart info={inc} name={ticker} type="ebitda" horizon={horizon}/>
       </div>
       <div className="chart">
-        <BarChart info={msft_cf1} name={"Microsoft"} type="cash_flow" horizon={horizon}/>
+        <BarChart info={cf} name={ticker} type="cash_flow" horizon={horizon}/>
       </div>
       <div className="chart">
-        <BarChart info={msft_bal1} name={"Microsoft"} type="debt" horizon={horizon}/>
+        <BarChart info={bal} name={ticker} type="debt" horizon={horizon}/>
       </div>
       <div className="chart">
-        <BarChart info={msft_bal1} name={"Microsoft"} type="stock" horizon={horizon}/>
+        <BarChart info={bal} name={ticker} type="stock" horizon={horizon}/>
       </div>
       </div>
-      } 
     </>
   )
 }
